0d4ab5c09e5af7e679f1cebe54f957613543d9a4
vkekrahs3737@gmail.com <vkekrahs3737@gmail.com>
2022-01-05 00:28:38 +0900

    22.01.05 이건호 기본 엔진 세팅 커밋

---
+++ GameEngineBase/PreCompile.h
@@ -0,0 +1,15 @@
+#pragma once
+
+//기본 계속해서 사용하게 될 헤더들을 여기에 모아둔다.
+//속성->미리 컴파일 된 헤더파일에서 적용 가능
+
+#include <Windows.h>
+
+#include <vector>
+#include <list>
+#include <map>
+#include <string>
+
+#include <io.h>
+#include <assert.h>
+#include <crtdbg.h>
