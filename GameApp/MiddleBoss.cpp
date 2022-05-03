55af3faa833133cb9f1e4209aad8e65cc156a68a
vkekrahs3737@gmail.com <vkekrahs3737@gmail.com>
2022-05-03 23:17:01 +0900

    22.05.03 이건호 미들보스 이미지/기초 클래스 커밋

---
+++ GameApp/MiddleBoss.cpp
@@ -0,0 +1,52 @@
+#include "PreCompile.h"
+#include "MiddleBoss.h"
+#include "GameEngine/GameEngineRenderer.h"
+#include "GameEngine/GameEngineImageRenderer.h"
+#include <GameEngine\GameEngineCollision.h>
+
+
+MiddleBoss::MiddleBoss() // default constructer 디폴트 생성자
+{
+
+}
+
+MiddleBoss::~MiddleBoss() // default destructer 디폴트 소멸자
+{
+
+}
+
+void MiddleBoss::Start()
+{
+
+}
+
+void MiddleBoss::Update(float _DeltaTime)
+{
+}
+
+void MiddleBoss::Idle()
+{}
+
+void  MiddleBoss::Wake()
+{}
+
+void  MiddleBoss::Walk()
+{}
+
+void  MiddleBoss::Turn()
+{}
+
+void  MiddleBoss::Attack()
+{}
+
+void  MiddleBoss::JumpReady()
+{}
+
+void  MiddleBoss::Jump()
+{}
+
+void  MiddleBoss::GetUp()
+{}
+
+void  MiddleBoss::Death()
+{}
---
+++ GameApp/MiddleBoss.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <GameEngine/GameEngineActor.h>
+#include <GameEngine/GameEngineFSM.h>
+
+
+// 분류 : 
+// 용도 : 
+// 설명 : 
+class MiddleBoss : public GameEngineActor
+{
+private:	// member Var
+
+public:
+	MiddleBoss(); // default constructer 디폴트 생성자
+	~MiddleBoss(); // default destructer 디폴트 소멸자
+
+	GameEngineImageRenderer* ImageRenderer;
+	GameEngineCollision* Collision;
+	GameEngineFSM StateManager_;
+
+	float4 MapBotCollisionColor;
+	float4 MapTopCollisionColor;
+	float4 MapLeftCollisionColor;
+	float4 MapRightCollisionColor;
+
+protected:		// delete constructer
+	MiddleBoss(const MiddleBoss& _other) = delete; // default Copy constructer 디폴트 복사생성자
+	MiddleBoss(MiddleBoss&& _other) = delete; // default RValue Copy constructer 디폴트 RValue 복사생성자
+
+private:		//delete operator
+	MiddleBoss& operator=(const MiddleBoss& _other) = delete; // default Copy operator 디폴트 대입 연산자
+	MiddleBoss& operator=(const MiddleBoss&& _other) = delete; // default RValue Copy operator 디폴트 RValue 대입연산자
+
+private:
+	void Start() override;
+	void Update(float _DeltaTime) override;
+
+private:
+	void Idle();
+	void Wake();
+	void Walk();
+	void Turn();
+	void Attack();
+
+	void JumpReady();
+	void Jump();
+	void GetUp();
+
+	void Death();
+
+public:
+	bool GetDamage;
+	int HP;
+
+	float MoveDistance;
+	float ImmuneTime;
+	float SpinTime;
+	float Speed;
+};
+
