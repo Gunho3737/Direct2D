489a024464f0827dcd8a4c6bb8b4841975895e50
vkekrahs3737@gmail.com <vkekrahs3737@gmail.com>
2022-05-11 00:50:57 +0900

    22.05.11 이건호 GroundWave 클래스 커밋

---
+++ GameApp/GroundWave.cpp
@@ -0,0 +1,25 @@
+#include "PreCompile.h"
+#include "GroundWave.h"
+#include "GameEngine/GameEngineRenderer.h"
+#include "GameEngine/GameEngineImageRenderer.h"
+#include <GameEngine\GameEngineCollision.h>
+
+
+GroundWave::GroundWave() // default constructer 디폴트 생성자
+{
+
+}
+
+GroundWave::~GroundWave() // default destructer 디폴트 소멸자
+{
+
+}
+
+void GroundWave::Start()
+{
+
+}
+
+void GroundWave::Update(float _DeltaTime)
+{
+}
