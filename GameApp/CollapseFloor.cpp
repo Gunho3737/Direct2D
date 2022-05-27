d62379ac444e2d37390970b124852429d42f45f4
vkekrahs3737@gmail.com <vkekrahs3737@gmail.com>
2022-05-27 18:25:11 +0900

    22.05.27 이건호 collapsefloorpieces 기초 커밋

--- GameApp/CollapseFloor.cpp
+++ GameApp/CollapseFloor.cpp
@@ -26,14 +26,16 @@ void CollapseFloor::Start()
 	Collision = CreateTransformComponent<GameEngineCollision>(int(ActorCollisionType::FLOOR));
 	Collision->GetTransform()->SetLocalScaling({1000.0f, 30.0f});
 	Collision->GetTransform()->SetLocalPosition({0.0f,140.0f,-10.0f });
+
 	{
-		CollapseFloorPieces.reserve(12);
-		for (int i = 0; i < 10; i++)
+		CollapseFloorPieces.reserve(20);
+		for (int i = 0; i < 15; i++)
 		{
 		GameEngineImageRenderer* ImageRenderer = nullptr;
 		ImageRenderer = CreateTransformComponent<GameEngineImageRenderer>(GetTransform());
 		int RockNumber = i % 4;
-		
+		ImageRenderer->GetTransform()->SetLocalScaling(float4{ 20.0f, 20.0f, 1.0f });
+
 		switch (RockNumber)
 		{
 		case 0:
@@ -51,10 +53,17 @@ void CollapseFloor::Start()
 		default:
 			break;
 		}
-		ImageRenderer->GetTransform()->SetLocalScaling(float4{20.0f, 20.0f, 1.0f});
+
 		ImageRenderer->Off();
 		CollapseFloorPieces.push_back(ImageRenderer);
 		}
+
+	//	for (int i = 0; i < 15; i++)
+	//	{
+	//		float number = (float)i;
+	//		CollapseFloorPieces[i]->GetTransform()->SetLocalPosition(float4{number, number, 1.0f});
+	//	}
+
 	}
 }
 
